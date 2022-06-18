2cc3190fd998d0a997a9d2e9736fbf66b9ca7b2b
Scarlettlp <scarlett.lara.p@gmail.com>
2022-06-18 13:41:42 -0500

    footer updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import './App.css';
-import Dictionary from "./Dictionary.js"
+import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>
-          Dictionary
-      </h1>
-      <p>
-        Definitions from <a href='https://dictionaryapi.dev/'>Free Dictionary API</a>
-      </p>
-      <main>
-      <Dictionary/>
-      </main>
-      <footer className="App-footer">Open source code by Scarlett Lara</footer>
-    </div>
+        <h1>Dictionary</h1>
+        <p>
+          Definitions from{" "}
+          <a href="https://dictionaryapi.dev/">Free Dictionary API</a>
+        </p>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          Open source code by Scarlett Lara{" "}
+          <a href="https://github.com/Scarlettlp/dictionary-react">GitHub</a>
+        </footer>
+      </div>
     </div>
   );
 }
