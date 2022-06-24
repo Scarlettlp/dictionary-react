1acc74cef676b3fbbf18ffad34693da2406b6180
Scarlettlp <scarlett.lara.p@gmail.com>
2022-06-24 16:12:08 -0500

    phonetics update

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   console.log(props.results);
@@ -7,8 +8,18 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
-         {props.results.phonetics[0].audio}
-        {props.results.phonetics[0].text}
+        {props.results.phonetics
+          .filter(function (phonetic) {
+            return phonetic.audio;
+          })
+          .map(function (phonetic, index) {
+            return (
+              <div key={index}>
+                <Phonetics phonetic={phonetic} />
+              </div>
+            );
+          })}
+
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
